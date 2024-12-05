977b012dd0618025fd352bf2d3953c39f0dffee6
Wi91 <will@LAPTOP-89HBOJFI.>
2024-12-05 12:34:30 +0000

    Added methods to service layer

--- src/main/java/com/northcoders/recordshopapi/service/RecordShopService.java
+++ src/main/java/com/northcoders/recordshopapi/service/RecordShopService.java
@@ -1,4 +1,17 @@
 package com.northcoders.recordshopapi.service;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecordShopService {
+
+List<Record> getAllAlbums();
+
+Optional<Record> getAlbumById(Long id);
+
+Record addAlbum(Record record);
+
+Record updateAlbumById(Long id, Record record);
+
+String deleteRecordById(Long id);
 }
