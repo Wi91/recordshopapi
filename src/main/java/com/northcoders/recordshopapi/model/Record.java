4ffd3124e12e7f3b8a58421fc0ff9977f980374d
Wi91 <will@LAPTOP-89HBOJFI.>
2024-12-05 12:10:52 +0000

    Feat: Populated Record Model class

--- src/main/java/com/northcoders/recordshopapi/model/Record.java
+++ src/main/java/com/northcoders/recordshopapi/model/Record.java
@@ -1,4 +1,40 @@
 package com.northcoders.recordshopapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serial;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+
 public class Record {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String albumTitle;
+
+    @Column
+    String artistName;
+
+    @Column
+    Genre genre;
+
+    @Column
+    String dateReleased;
+
+    @Column
+    int stock;
 }
